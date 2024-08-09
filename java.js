b7a4c81ca7a5053e13457dcb4a7a6f9b36625127
Mo Sharif <kacfariiso@gmail.com>
2024-08-10 02:39:32 +0300

    Add new files and update existing code
    - Added new PSD file: 'ina hodan.psd'
    - Created 'java form code.txt' with new Java code
    - Updated 'index.html' for content or structural changes
    - Enhanced JavaScript functionality in 'java.js'
    - Modified 'style.css' for improved styling

--- java.js
+++ java.js
@@ -1,1 +1,21 @@
-ccc
+/*const form = document.querySelector("form");
+
+function sendEmail() {
+Email.send({
+Host: "smtp.elasticemail.com",
+Username: "jerrymohamed18@gmail.com",
+Password: "F56C4389ABEA754F5D24733DAAE73739C2AE",
+To: 'jerrymohamed18@gmail.com',
+From: "jerrymohamed18@gmail.com",
+Subject: "This is the subject",
+Body: "And this is the body"
+}). then(
+message => alert(message)
+);
+
+}
+
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+sendEmail();
+}); */
